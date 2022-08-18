2ca92394cc64d785b6f634fec63c019888ff13f6
Christopher Perez <chistoperez@gmail.com>
2022-08-18 09:36:18 -0500

    laterales

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,15 +1,19 @@
 import React, { useContext } from "react";
 import NavBar from "../components/NavBar";
 import { NavBarContext } from "../context/NavBarContext";
+import { BsCartX } from "react-icons/bs";
 
 const Cart = () => {
   const { setNavbarActive } = useContext(NavBarContext);
   setNavbarActive(0);
   return (
-    <>
-      Cart
+    <div className="bg-secondary-100/30 h-screen flex flex-col items-center justify-center ">
+      <BsCartX size={200} className="text-secondary-100  " />
+      <h1 className="text-secondary-100 text-center text-5xl pt-10 font-bold">
+        Tu carrito está vacío.
+      </h1>
       <NavBar />
-    </>
+    </div>
   );
 };
 
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import NavBar from "../components/NavBar";
 import { NavBarContext } from "../context/NavBarContext";
+import { CgProfile } from "react-icons/cg";
 
 const Profile = () => {
   const { date, actualWeekday, calendarActive, setNavbarActive, active, time } =
@@ -8,13 +9,22 @@ const Profile = () => {
   setNavbarActive(2);
   return (
     <>
-      <div>
-        {actualWeekday[calendarActive]}- {date + calendarActive}
+      <div className="bg-secondary-100/30 h-screen flex flex-col items-center justify-center ">
+        <CgProfile size={200} className="text-secondary-100  " />
+        <h1 className="text-secondary-100 text-center text-5xl pt-10 font-bold">
+          Datos seleccionados
+        </h1>
+        <p className="text-secondary-100 text-center text-2xl pt-10">
+          Día: {actualWeekday[calendarActive]} - {date + calendarActive}
+        </p>
+        <p className="text-secondary-100 text-center text-2xl pt-10">
+          Horario de entrega: {time}
+        </p>
+        <p className="text-secondary-100 text-center text-2xl pt-10">
+          Servicio: {active}
+        </p>
+        <NavBar />
       </div>
-      <div>{time}</div>
-      <div>{active}</div>
-
-      <NavBar />
     </>
   );
 };
